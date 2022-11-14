cf7f600ccb18bb9390d585ad65190f162ff22bb7
Ivan Hayel <i.hayel.dev@gmail.com>
2022-11-14 21:47:42 +0300

    No args constructor + setters added to DTO.

--- src/main/java/com/modsen/api/model/dto/EventDto.java
+++ src/main/java/com/modsen/api/model/dto/EventDto.java
@@ -7,11 +7,15 @@ import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import org.springframework.lang.Nullable;
 
+@NoArgsConstructor(force = true)
 @AllArgsConstructor
 @Getter
+@Setter
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class EventDto implements Serializable {
   @Nullable Long id;
